2cd91ddd15c254a4e26782591cdffa568bbf4098
Felix-Jacob <jacobfelix51@gmail.com>
2022-08-19 10:32:23 +0000

    bugfix

--- db_mqtt_client/index.js
+++ db_mqtt_client/index.js
@@ -5,4 +5,12 @@ console.log('attempting to connect to broker');
 
 client.on('connect', () => {
   console.log("ejfskld;flkjfdslkfjsdlksdfj");
+  client.subscribe('/test', (err) => {
+    console.log('subbed');
+  });
+});
+
+client.on('message', ()=> {
+  console.log('dls;fjadlkfjdskl;fjdsalk;fjdsalk;fdjsflkasdfjadkl');
+  //console.log(message.toString());
 });
