69aa517c4a54b811fb96604112a60f914e397eef
Felix-Jacob <jacobfelix51@gmail.com>
2022-08-21 20:58:38 +0000

    can install nodem locally

--- ydb_rest_service/index.js
+++ ydb_rest_service/index.js
@@ -5,21 +5,21 @@ const app = express();
 app.use(express.json());
 
 app.listen(4000);
-
 app.post('/expired', (req, res) => {
+  // check recieved JSON
   if(!req.body.articleName){
     res.status(400).send('wrong data');
     return;
   }
+  // check if article with said name exists. people could otherwise spam the db
+  res.send('hi' + data({global: req.body.articleName}));
   ydb.set({global: req.body.articleName, subscripts:['active'], data:0}, (error, result) => {
     if(error){
-      res.status(500).send('ydb_error");
+      res.status(500).send('ydb_error');
       return;
     }
-    res.status(200).send(result);
+    res.status(200).send("hi");
   });
-  else
-    res.send('idk');
 });
 
 ydb_status = ydb.open(
--- ydb_rest_service/test_scripts/curls.sh
+++ ydb_rest_service/test_scripts/curls.sh
@@ -1,1 +1,1 @@
-curl -X POST localhost:4000/expired -H 'Content-Type: application/json' -d '{"hello":"world"}'
+curl -X POST localhost:4000/expired -H 'Content-Type: application/json' -d '{"articleName":"ot"}'
