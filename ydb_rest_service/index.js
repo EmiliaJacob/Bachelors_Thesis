6810ece6747db54bdad01a4aa2d563d4d6b2ca1b
Felix-Jacob <jacobfelix51@gmail.com>
2022-08-23 14:56:28 +0000

    check if article already inactive

--- ydb_rest_service/index.js
+++ ydb_rest_service/index.js
@@ -6,12 +6,10 @@ app.use(express.json());
 
 app.listen(4000);
 
+
 app.post('/expired', (req, res) => {
 
-  ydb.set({global: 'ot', subscripts: ['active'], data: 0});
-  ydb.set({global: 'ot', subscripts: ['price'], data: 700});
 
-  // check recieved JSON
   if(!req.body.articleName){
     res.status(400).send('wrong data');
     return;
@@ -19,13 +17,17 @@ app.post('/expired', (req, res) => {
 
   ydb_data_result = ydb.data({global: req.body.articleName}); 
 
-  // check if article with said name exists. people could otherwise spam the db
   if(ydb_data_result.defined == 0) {
     res.status(400).send("article not found");
     return;
   }
 
-  // set active property to false which actives a trigger in ydb
+  current_article_status = ydb.get({global: req.body.articleName, subscripts: ['active']}).data;
+  if(current_article_status == 0) {
+    res.status(400).send('article already inactice');
+    return;
+  }
+
   ydb.set({global: req.body.articleName, subscripts:['active'], data:0}, (error, result) => {
     if(error){
       res.status(500).send('ydb_error');
@@ -43,3 +45,6 @@ ydb_status = ydb.open(
 );
 
 console.log(ydb_status);
+
+ydb.set({global: 'ot', subscripts: ['active'], data: 1});
+ydb.set({global: 'ot', subscripts: ['price'], data: 700});
