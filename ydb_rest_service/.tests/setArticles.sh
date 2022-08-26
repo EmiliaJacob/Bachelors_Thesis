127aeaccaa02c44ed809272a91a6bbcaa6e5833b
Felix-Jacob <jacobfelix51@gmail.com>
2022-08-26 14:30:52 +0200

    one article done

--- ydb_rest_service/.tests/setArticles.sh
+++ ydb_rest_service/.tests/setArticles.sh
@@ -3,4 +3,13 @@ id_1=1
  
 curl -X POST localhost:4000/set \
 	-H 'Content-Type: application/json' \
-	-d '{"global":"articles", "subscripts":["id"], "data":"$id_0"}'
+	-d '{"global":"articles", "subscripts":["$id_0"], "data":"$id_0"}'
+	
+curl -X POST localhost:4000/set \
+	-H 'Content-Type: application/json' \
+	-d '{"global":"articles", "subscripts":["$id_0", "name"], "data":"Tulips"}'
+	
+curl -X POST localhost:4000/set \
+	-H 'Content-Type: application/json' \
+	-d '{"global":"articles", "subscripts":["$id_0", "price"], "data":"3.99"}'
+	
