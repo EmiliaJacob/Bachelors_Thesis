730dd5dd9bfe37091a06398009660670869f7b5a
Felix-Jacob <jacobfelix51@gmail.com>
2022-08-26 14:35:06 +0200

    refactor

--- ydb_rest_service/index.js
+++ ydb_rest_service/index.js
@@ -45,7 +45,7 @@ app.post('/set', (req, res) => {
         res.status(500).send(error);
         return;
       }
-      res.status(200).send(`set: ${reqGlobal}(${reqSubscripts})=${reqData}`);
+      res.status(200).send(`set: ${reqGlobal}(${reqSubscripts})=${reqData}\n`);
     });
   }
   else {
@@ -54,7 +54,7 @@ app.post('/set', (req, res) => {
         res.status(500).send(error);
         return;
       }
-      res.status(200).send(`set: ${reqGlobal}=${reqData}`);
+      res.status(200).send(`set: ${reqGlobal}=${reqData}\n`);
     });
   }
 
