c435e3753940dbfa07d58b318dcf5f866cc7f38c
Felix-Jacob <jacobfelix51@gmail.com>
2022-08-26 14:24:10 +0200

    new test scripts

---
+++ ydb_rest_service/.tests/setArticles.sh
@@ -0,0 +1,6 @@
+id_0=0
+id_1=1
+ 
+curl -X POST localhost:4000/set \
+	-H 'Content-Type: application/json' \
+	-d '{"global":"articles", "subscripts":["id"], "data":$id_0}'
--- ydb_rest_service/.tests/testTrigInvoke.sh
+++ ydb_rest_service/.tests/testTrigInvoke.sh
@@ -1,1 +1,2 @@
 curl -X POST localhost:4000/set -H 'Content-Type: application/json' -d '{"global":"test", "data":"fdsf"}'
+
