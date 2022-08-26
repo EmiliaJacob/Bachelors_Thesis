3a556a4cf252a78240698049f26771f83fad2edd
Felix-Jacob <jacobfelix51@gmail.com>
2022-08-26 14:27:56 +0200

    fix

--- ydb_rest_service/.tests/setArticles.sh
+++ ydb_rest_service/.tests/setArticles.sh
@@ -3,4 +3,4 @@ id_1=1
  
 curl -X POST localhost:4000/set \
 	-H 'Content-Type: application/json' \
-	-d '{"global":"articles", "subscripts":["id"], "data":$id_0}'
+	-d '{"global":"articles", "subscripts":["id"], "data":"$id_0"}'
