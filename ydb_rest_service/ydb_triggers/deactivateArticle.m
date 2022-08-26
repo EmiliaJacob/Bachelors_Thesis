9b8da87f352451a28ea1d6d8a2142d58da8deeb2
Felix-Jacob <jacobfelix51@gmail.com>
2022-08-26 16:17:12 +0000

    refactoring

--- ydb_rest_service/ydb_triggers/deactivateArticle.m
+++ ydb_rest_service/ydb_triggers/deactivateArticle.m
@@ -1,7 +1,7 @@
- if $ZTVALUE=0  do
- .set mosquittoFifo="/app/mosquittoFifo"
- .open mosquittoFifo:fifo
- .set topic="/deactivated"
- .set message=articleId
- .use mosquittoFifo write topic_" "_message,!
- .close mosquittoFifo
+    if $ZTVALUE=0  do
+    .set mosquittoFifo="/app/mosquittoFifo"
+    .open mosquittoFifo:fifo
+    .set topic="/deactivated"
+    .set message=articleId
+    .use mosquittoFifo write topic_" "_message,!
+    .close mosquittoFifo
